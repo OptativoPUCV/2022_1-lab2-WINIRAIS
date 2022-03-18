b384412bcc47f6fc8185bdaaac0aee326a4393cd
Your Name <you@example.com>
2022-03-18 18:00:32 +0000

    new try

--- list.c
+++ list.c
@@ -71,7 +71,7 @@ void *prevList(List *list) {
 }
 
 void pushFront(List *list, void *data) { 
-    Node *nodo = createNode(data);
+    //Node *nodo = createNode(data);
   }
 
 void pushBack(List *list, void *data) {
